f6bc7903348ffc4cbd30cdc526e858c888b2be39
ox.sugar <ms.oksana@mail.ru>
2021-03-24 12:04:20 +0300

    +Jaeger

--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -3,9 +3,29 @@ import java.util.Scanner;
 public class CalculatorTest {
 
     public static void main(String[] args) {
-       do {
-            Calculator clclt = new Calculator();
-            Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+        int res = calculateData();
+        System.out.println("Результат равен: " + res);
+
+        System.out.print("Хотите продолжить вычисления? [yes/no]:");
+        String userAnswer = in.nextLine();
+        System.out.println("0 " + userAnswer);
+        if (userAnswer == "yes") {
+            System.out.println("1" + userAnswer);
+            res = calculateData();
+            System.out.println("Результат равен: " + res);
+        } else if (userAnswer == "no") {
+        } else {
+            while(userAnswer != "yes" || userAnswer != "no") {
+            System.out.print("Хотите продолжить вычисления? [yes/no]:");
+            userAnswer = in.nextLine();
+            System.out.println("2" + userAnswer);
+            }
+        }
+    }
+    public static int calculateData() {
+        Scanner in = new Scanner(System.in);
+        Calculator clclt = new Calculator();
             System.out.print("Введите первое число: ");
             int num1 = in.nextInt();
             System.out.print("Введите знак математической операции: ");
@@ -13,9 +33,6 @@ public class CalculatorTest {
             System.out.print("Введите второе число: ");
             int num2 = in.nextInt();
             int res = clclt.calculateData(num1, num2, operation);
-            System.out.println("Результат: " + res);
-            System.out.print("Хотите продолжить вычисления? [yes/no]:");
-            String userAnswer = in.nextLine();
-        } while (userAnswer == "yes");
-    }
+            return  res;
+        }
 }
