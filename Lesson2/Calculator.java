da6c05c97b820dc1b0a086815ed8dfc980b64370
ox.sugar <ms.oksana@mail.ru>
2021-03-26 17:02:09 +0300

    Доделать

--- Lesson2/Calculator.java
+++ Lesson2/Calculator.java
@@ -5,7 +5,36 @@ public class Calculator {
     private int num2;
     private int result;
 
-    public int calculateData(int num1, int num2, char operation) {
+    public void setNum1(int num1){
+        this.num1 = num1;
+    }
+
+    public void setOperation(char operation){
+        this.operation = operation;
+    }
+
+    public void setNum2(int num2){
+        this.num2 = num2;
+    }
+
+    public int getNum1(){
+        return num1;
+    }
+
+    public char getOperation(){
+        return operation;
+    }
+
+    public int getNum2(){
+        return num2;
+    }
+
+    public int calculate(int num1, int num2, char operation) {
+
+//System.out.println("num1: " + num1);
+//System.out.println("num2: " + num2);
+//System.out.println("operation: " + operation);
+
         switch(operation) {
             case '+': 
                 result = num1 + num2;
--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -4,34 +4,44 @@ public class CalculatorTest {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int res = calculateData();
+        Calculator clclt = new Calculator();
+        //int res = clclt.calculate(num1, num2, operation);
+        int res = clclt.calculate(clclt.getNum1(), clclt.getNum2(), clclt.getOperation());
+        //System.out.println("num1: " + clclt.getNum1());
+        //System.out.println("num2: " + clclt.getNum2());
+        //System.out.println("operation: " + clclt.getOperation());
         System.out.println("Результат равен: " + res);
-
-        System.out.print("Хотите продолжить вычисления? [yes/no]:");
-        String userAnswer = in.nextLine();
-
-        if (userAnswer.equals("yes")) {
-            res = calculateData();
-            System.out.println("Результат равен: " + res);
-        } else if (userAnswer.equals("no")) {
-        } else {
-            while(userAnswer.equals("yes") || !userAnswer.equals("no")) {
+        //System.out.println("Результат равен: " + res);
+        String userAnswer;
+        
+        do {
             System.out.print("Хотите продолжить вычисления? [yes/no]:");
             userAnswer = in.nextLine();
-            }
-        }
+            } while (!userAnswer.equals("yes") || !userAnswer.equals("no"));
 
-        int calculateData() {
-            Scanner in = new Scanner(System.in);
-            Calculator clclt = new Calculator();
-            System.out.print("Введите первое число: ");
-            int num1 = in.nextInt();
-            System.out.print("Введите знак математической операции: ");
-            char operation = in.next(".").charAt(0);
-            System.out.print("Введите второе число: ");
-            int num2 = in.nextInt();
-            int res = clclt.calculateData(num1, num2, operation);
-            return  res;
+        if (userAnswer.equals("yes")) {
+            inputOperands();
+            System.out.println("Результат равен: " + res);
         }
+    }
+
+    public static void inputOperands() {
+        CalculatorTest val = new CalculatorTest();
+        Calculator values = new Calculator();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int num1 = in.nextInt();
+        System.out.print("Введите знак математической операции: ");
+        char operation = in.next(".").charAt(0);
+        System.out.print("Введите второе число: ");
+        int num2 = in.nextInt();
+        values.setNum1(num1);
+        values.setOperation(operation);
+        values.setNum2(num2);
+        //System.out.println("num1: " + num1);
+        //System.out.println("num2: " + num2);
+        //System.out.println("operation: " + operation);
+        //int res = clclt.calculateData(num1, num2, operation);
+        //return values;
     }
 }
