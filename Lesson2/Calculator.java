a2c2ee13d0078dcfa61b4f20ebd6a398d6b0b988
ox.sugar <ms.oksana@mail.ru>
2021-03-30 13:53:29 +0300

    Отработка калькулятора

--- Lesson2/Calculator.java
+++ Lesson2/Calculator.java
@@ -1,21 +1,7 @@
-import java.util.Scanner;
 
 public class Calculator {
 
     private int result;
-    Scanner in = new Scanner(System.in);
-
-    public int getNum() {
-        System.out.print("Введите число: ");
-        int num = in.nextInt();
-        return num;
-    }
-
-    public char getOperand() {
-        System.out.print("Введите знак математической операции: ");
-        char operation = in.next(".").charAt(0);
-        return operation;
-    }
 
     public int calculate(int num1, int num2, char operation) {
         switch(operation) {
