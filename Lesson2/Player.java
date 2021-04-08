121e78a899bac28345d1598223f78934240991ac
ox.sugar <ms.oksana@mail.ru>
2021-04-08 16:01:23 +0300

    Попытка не пытка

--- Lesson2/Player.java
+++ Lesson2/Player.java
@@ -7,7 +7,15 @@ public class Player {
         this.name = name;
     }
 
-        public String toString() {
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String toString() {
         return name;
     }
 }
