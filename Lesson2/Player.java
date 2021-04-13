1ef31f994b521d305e32466ca175a207aac747a0
ox.sugar <ms.oksana@mail.ru>
2021-04-13 20:20:39 +0300

    HomeWork

--- Lesson2/Player.java
+++ Lesson2/Player.java
@@ -1,18 +1,9 @@
 public class Player {
     
     private String name;
-    //private int number;
 
     Player(String name) {
         this.name = name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public String toString() {
